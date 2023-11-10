fb179f5f9b4c46defc0ea243be9d02ea3ac0663b
Gambhir ⚡ <gambhir786191ss@gmail.com>
2023-11-11 04:23:22 +0530

    init: Derek Banas Rust Tutorial YT

---
+++ DerekBanas-YT/rust_tutotial/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(unused)]
+
+use std::io;     // input and output
+use rand::Rng;  // get random number
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+// Type your name and get the greeting.
+// fn main() {
+//     println!("What is your name?");
+//     let mut name = String::new();
+//     let greeting  = "Nice to meet you!";
+//     io::stdin().read_line(&mut name)
+//         .expect("Didn't Receive Input");
+//     println!("hello {}! {}", name.trim_end(), greeting); // .trim_end del new line of name var
+// }
+
+
+
